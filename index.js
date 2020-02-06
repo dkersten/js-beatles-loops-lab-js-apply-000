94e7c119bd00475456f983eaf2923ffc941fb1f2
Daniel Kersten <dkersten1994@gmail.com>
2020-02-06 10:59:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(mus, inst) {
+  var emptArray;
+   for (i = 0; i < mus.length; i++) {
+     emptArray = `${mus[i]} plays ${inst[i]}. `;
+   }
+   return emptArray;
+}
+
+theBeatlesPlay(musicians, instruments);
+
+
