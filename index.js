87e3527debfc66109010e3a1e5f20dc3eaa7fb1e
Daniel Kersten <dkersten1994@gmail.com>
2020-02-06 11:08:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(arrayM, arrayI) {
   var emptArray;
-  var i;
-   for (i = 0; i < arrayM.length; i++) {
+   for ( var i = 0; i < arrayM.length; i++) {
      emptArray += `${arrayM[i]} plays ${arrayI[i]} `;
    }
    return emptArray;
