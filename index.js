1f03192c006d28bde641185c6b6d5cf7b924bbcd
Daniel Kersten <dkersten1994@gmail.com>
2020-02-06 11:02:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-function theBeatlesPlay(mus, inst) {
+function theBeatlesPlay(arrayM, arrayI) {
   var emptArray;
    for (i = 0; i < mus.length; i++) {
-     emptArray = `${mus[i]} plays ${inst[i]}. `;
+     emptArray = `${arrayM[i]} plays ${arrayI[i]}.`;
    }
    return emptArray;
 }
